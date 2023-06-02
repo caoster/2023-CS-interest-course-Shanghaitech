3c58a71c20a9ede97a481fb88559f84447a14db9
caoster <caoster@icloud.com>
2023-06-02 20:14:14 +0800

    Basic UI and logic

--- main.py
+++ main.py
@@ -2,34 +2,19 @@ import random
 import time
 
 from cardUI import display, start
+from cardUI_extension import game, cardUI_help
+
+NUM_CARDS = 20
 
 
 def new_card():
-    return [[random.randint(2, 14), random.randint(0, 3)] for _ in range(30)]
+    all_cards = [[card // 4 + 2, card % 4] for card in random.sample(range(52), NUM_CARDS * 2)]
+    return [all_cards[:NUM_CARDS], all_cards[NUM_CARDS:]]
 
 
-def sort_all(cards: list):
-    return sorted(cards, key=lambda x: x[0] * 4 + x[1])
+def agent(my_card, opponent, on_board):
+    pass
 
 
-def search_all(cards: list, target_card: list):
-    cards = sort_all(cards)
-    result = []
-    i = 0
-    while i < len(cards) - 1:
-        if cards[i] == target_card[0] and cards[i + 1] == target_card[1]:
-            result.append([cards[i], cards[i + 1]])
-            i += 1
-        i += 1
-    return result
-
-
-time.sleep(2)
-for i in range(1, 5):
-    a = []
-    for j in range(52):
-        a.append([random.randint(2, 14), random.randint(0, 3)])
-        display(i, a)
-time.sleep(1)
-
-start(new_card, sort_all, search_all, [[3, 1], [3, 2]])
+game(new_card, None, None)
+time.sleep(10)
