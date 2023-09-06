eea9b938a0c17878fa669ef34fc6b9570bce4d1d
caoster <caoster@icloud.com>
2023-09-07 01:18:14 +0800

    new treasureUI

---
+++ treasureUI.py
@@ -0,0 +1,131 @@
+from enum import Enum
+import tkinter
+from tkinter import Tk, Canvas
+
+
+class PixelType(Enum):
+    ROAD = 0
+    WALL = 1
+    EXIT = 2
+    START = 3
+    MOB = 4
+
+
+class Treasure:
+    def __init__(self):
+        self.map = [
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+        ]
+        # Transpose!
+        self.map = list(map(list, zip(*self.map)))
+        self.map = [[PixelType(j) for j in i] for i in self.map]
+        self.size = len(self.map), len(self.map[0])
+        self.entrance = (0, 0)
+        self.exit = (18, 9)
+        self.mobs = [{"location": (5, 0), "cost": 100}, {"location": (7, 0), "cost": 150}]
+
+        self._disp: _DISP = _DISP(self)
+        self._init_disp()
+
+        # TODO:
+        self.update_player(1, 0)
+
+    def _init_disp(self):
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                self._disp.update(i, j, self.map[i][j])
+
+    def update_player(self, x, y):
+        self._disp.update_player(x, y)
+
+    def update_mobs(self):
+        pass
+
+    def start(self):
+        self._disp.start()
+
+
+class _DISP:
+    def __init__(self, treasure: Treasure):
+        self.root = Tk(className="Treasure")
+        self.root.resizable(False, False)
+        # self.root.bind("<Escape>", lambda _: self.root.destroy())
+        self.root.geometry("850x400")
+        self.canvas = Canvas(self.root, width=850, height=400, background="#000")
+        self.canvas.pack()
+        self.canvas.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        self.cells = []
+        for x in range(18):
+            self.cells.append([])
+            for y in range(9):
+                x_start, y_start, x_end, y_end = self._get_position(x, y)
+                rect = self.canvas.create_rectangle(x_start, y_start, x_end, y_end, fill="gray55")
+                self.cells[-1].append(rect)
+
+        # self.score_notice = self.canvas.create_text(850, 10, text="探索步数", fill="gray85",
+        #                                             font=('Helvetica', '15', 'bold'), anchor=tkinter.NE)
+        # self.score = self.canvas.create_text(850, 40, text=0, fill="gray85",
+        #                                      font=('Helvetica', '21', 'bold'), anchor=tkinter.NE)
+        #
+        # self.length_notice = self.canvas.create_text(850, 80, text="路径长度", fill="green",
+        #                                              font=('Helvetica', '15', 'bold'),
+        #                                              anchor=tkinter.NE, tags="length")
+        # self.length = self.canvas.create_text(850, 110, text=0, fill="green",
+        #                                       font=('Helvetica', '21', 'bold'),
+        #                                       anchor=tkinter.NE, tags="length")
+        self.treasure = treasure
+
+        self.player = None
+        self.mobs = []
+
+    @staticmethod
+    def _get_position(x, y, small=False):
+        side = 40
+        offset = 20
+        smaller = 8
+        if small:
+            return x * side + offset + smaller, y * side + offset + smaller, \
+                   x * side + offset + side - smaller, y * side + offset + side - smaller
+        else:
+            return x * side + offset, y * side + offset, x * side + offset + side, y * side + offset + side
+
+    def start(self):
+        self.root.mainloop()
+
+    def update(self, x: int, y: int, scheme: PixelType):
+        if scheme == PixelType.ROAD:
+            self.canvas.itemconfigure(self.cells[x][y], fill="gray85")
+        elif scheme == PixelType.WALL:
+            self.canvas.itemconfigure(self.cells[x][y], fill="gray15")
+        elif scheme == PixelType.START:
+            self.canvas.itemconfigure(self.cells[x][y], fill="gold")
+        elif scheme == PixelType.EXIT:
+            self.canvas.itemconfigure(self.cells[x][y], fill="red")
+        elif scheme == PixelType.MOB:
+            self.canvas.itemconfigure(self.cells[x][y], fill="green")
+
+    # def update_score(self, score: int):
+    #     self.canvas.itemconfigure(self.score, text=score)
+    #
+    # def update_length(self, length: int):
+    #     self.canvas.itemconfigure(self.length, text=length)
+
+    def update_player(self, x, y):
+        self.canvas.delete(self.player)
+        x_start, y_start, x_end, y_end = self._get_position(x, y, True)
+        self.player = self.canvas.create_rectangle(x_start, y_start, x_end, y_end, fill="blue")
+
+    def update_mobs(self, mobs):
+        pass
+
+
+Treasure().start()
